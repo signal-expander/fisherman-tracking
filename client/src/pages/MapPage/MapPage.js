bf66cefab114b33bb0f61622cc80e493fa44fbf6
Jay Tamayo <tamayojay5@gmail.com>
2020-08-22 13:11:49 +0800

    message

--- client/src/pages/MapPage/MapPage.js
+++ client/src/pages/MapPage/MapPage.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import leafRed from './icons/leaf-red.png';
+import leafShadow from './icons/leaf-shadow.png';
 
-const MapPage = () => {
+class MapPage extends Component {
 
-    return (
-        <>
-            <div className="container-fluid bg-info">
-                <div className="row">
-                    <div className="col-md-12">
-                        Map Layout here (use leaflet)
-                    </div>
-                </div>
+    state = {
+        redIcon: {
+            lat: 8.5352,
+            lng: 124.5653,
+        },
+        zoom: 13,
+    }
+
+    redIcon = L.icon({
+        iconUrl: leafRed,
+        shadowUrl: leafShadow,
+        iconSize:     [38, 95], 
+        shadowSize:   [50, 64], 
+        iconAnchor:   [22, 94], 
+        shadowAnchor: [4, 62],  
+        popupAnchor:  [-3, -76] 
+        
+
+    })
+
+    render() {
+        const position = [this.state.redIcon.lat, this.state.redIcon.lng];
+        return (
+            <div className="container-fluid">
+
+                <Map style={{height: "580px" , width: "880px"}} center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={position} icon={this.redIcon}>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    </Marker>
+                </Map>
+
+
+
             </div>
-        </>
-    )
+        )
+    }
 }
 
-export {
-    MapPage
-};
-
+export default MapPage
 
